3863a8ea8b387bccb8475bafb72aedd497b55da6
BennettJack <97344648+BennettJack@users.noreply.github.com>
2024-09-24 12:28:07 +0100

    Added Identity and DB context

---
+++ AMLBackend/Data/Context/AmlDbContext.cs
@@ -0,0 +1,67 @@
+﻿using AmlBackend.Data.Models.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmlBackend.Data.Context;
+
+public class AmlDbContext : IdentityDbContext<User>
+{
+    public AmlDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+    
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        List<IdentityRole> roles = new()
+        {
+            new IdentityRole()
+            {
+                Name = "Member",
+                NormalizedName = "MEMBER"
+            },
+            new IdentityRole()
+            {
+                Name = "BranchLibrarian",
+                NormalizedName = "BRANCHLIBRARIAN"
+            },
+            new IdentityRole()
+            {
+                Name = "BranchManager",
+                NormalizedName = "BRANCHMANAGER"
+            },
+            new IdentityRole()
+            {
+                Name = "CallOperator",
+                NormalizedName = "CALLOPERATOR"
+            },
+            new IdentityRole()
+            {
+                Name = "Accountant",
+                NormalizedName = "ACCOUNTANT"
+            },
+            new IdentityRole()
+            {
+                Name = "PurchaseManager",
+                NormalizedName = "PURCHASEMANAGER"
+            },
+            new IdentityRole()
+            {
+                Name = "Administrator",
+                NormalizedName = "ADMINISTRATOR"
+            },
+            new IdentityRole()
+            {
+                Name = "SystemAdministrator",
+                NormalizedName = "SYSTEMADMINISTRATOR"
+            },
+        };
+
+        builder.Entity<IdentityRole>().HasData(roles);
+    }
+}
--- AMLBackend/Program.cs
+++ AMLBackend/Program.cs
@@ -1,21 +1,70 @@
+using AmlBackend.Data.Context;
+using AmlBackend.Data.Models.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("AML API V1", new OpenApiInfo { Title = "AML API", Version = "1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+//Add the database context
+builder.Services.AddDbContext<AmlDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Add identity user and password requirements
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 10;
+        options.Password.RequireUppercase = true;
+    })
+    .AddEntityFrameworkStores<AmlDbContext>();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
