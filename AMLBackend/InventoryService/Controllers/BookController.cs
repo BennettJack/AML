5bbe5f5670c021d52f7235011bc9946ba5c3d2a4
SHU-Luke <116874437+SHU-Luke@users.noreply.github.com>
2024-11-11 13:54:58 +0000

    Book Controller

---
+++ AMLBackend/InventoryService/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using InventoryService2.Data;
+using InventoryService2.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly InventoryDbContext _context;
+
+        public BookController(InventoryDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        [Route("NewBook")]
+        public async Task<IActionResult> NewBook([FromBody] Book book)
+        {
+            await _context.AddAsync(book);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
