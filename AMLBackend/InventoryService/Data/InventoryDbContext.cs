8df95ce5b034299f380847b9740a70aa94307efa
SHU-Luke <116874437+SHU-Luke@users.noreply.github.com>
2024-11-11 13:20:27 +0000

    InventoryServiceInitial
    initial inventory service upload

---
+++ AMLBackend/InventoryService/Data/InventoryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using InventoryService2.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService2.Data
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+        : base(options)
+
+    {
+
+    }
+
+
+
+
+        public DbSet<Book> Books { get; set; }
+
+}
+}
---
+++ AMLBackend/InventoryService/Data/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService2.Data.Models
+{
+    public class Book
+    {
+        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+        public Book() { }
+    }
+}
